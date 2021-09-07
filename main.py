b8c555c82e55bc38909ae13491692de6386d7332
PrathamMittal17 <prathammittal172002@gmail.com>
2021-09-07 19:55:28 +0530

    added a new optional parameter

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def getData(city, path, data_qty=3):
 
     # getting data
     print(len(desc_box))
-    for i in range(0, len(desc_box)):
+    for i in range(10, len(desc_box)):
         try:
             price = ''
             bhk = ''
